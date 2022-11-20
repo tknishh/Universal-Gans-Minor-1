05be8cbce15f5bf545c14fbefe83f1b7b302e1ef
vaniseth <74897823+vaniseth@users.noreply.github.com>
2022-11-20 21:33:16 +0530

    modified

--- create_grid.py
+++ create_grid.py
@@ -1,6 +1,12 @@
 import sys
+import glob
 from PIL import Image
 
 pattern = sys.argv[1]
 rows = int(sys.argv[2])
 cols = int(sys.argv[3])
+filenames = glob.glob(pattern)
+
+images = [Image.open(name).resize((224,224)) for name in filenames]
+
+newImage = Image.new('RGB', (cols*224, rows*224))

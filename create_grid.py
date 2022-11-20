88227339f654c86478a206298be4efe5eba0bb39
vaniseth <74897823+vaniseth@users.noreply.github.com>
2022-11-20 21:36:49 +0530

    modified grid

--- create_grid.py
+++ create_grid.py
@@ -10,3 +10,17 @@ filenames = glob.glob(pattern)
 images = [Image.open(name).resize((224,224)) for name in filenames]
 
 newImage = Image.new('RGB', (cols*224, rows*224))
+
+i=0
+for y in range(rows):
+    if i >= len(images):
+        break
+    y *= 224
+    for x in range(cols):
+        x *= 224
+        img = images[i]
+        newImage.paste(img, (x,y,x+224, y+224))
+        print ('paste:',x,y)
+        i += 1
+
+newImage.save('grid.png')

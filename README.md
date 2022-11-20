6d0d38a45870e26fe3811088187849a6bcab46ea
Tanish Khandelwal <76485783+tknishh@users.noreply.github.com>
2022-11-20 20:47:33 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Universal-Gans-Minor-1
+ Sem 5 - Minor 1 - Projectno. 36 - JUET, Guna
---
+++ crop_imgs.py
@@ -0,0 +1,32 @@
+from PIL import Image
+import os
+from multiprocessing import Pool
+from tqdm import tqdm
+from functools import partial
+from os import cpu_count
+import numpy as np
+
+DATA_DIR = "./data"
+OUTPUT_DIR = "./images"
+
+def resize_image(image_name):
+    image_path =  os.path.join(DATA_DIR, image_name)
+    im = Image.open(image_path)
+    width,height = im.size
+
+    new_width = min(width,height)
+    new_height = min(width,height)
+
+    left = int(np.ceil((width - new_width)/2))
+    top = int(np.ceil((height - new_height)/2))
+    right = int(np.ceil((width + new_width)/2))
+    bottom = int(np.ceil((height + new_height)/2))
+
+    im = im.crop((left, top, right, bottom))
+    im.save(os.path.join(OUTPUT_DIR, image_name))
+
+paths = os.listdir(DATA_DIR)
+with Pool(processes = cpu_count() - 1) as p:
+    with tqdm(total=len(paths)) as pbar:
+        for v in p.imap_unordered(partial(resize_image), paths):
+            pbar.update()
